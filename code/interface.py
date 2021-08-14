896e86774551e66e5a79b7553a2b66e54532734f
myielin <mfernandamf114@gmail.com>
2021-08-14 16:15:08 -0300

    interface inicial

--- code/interface.py
+++ code/interface.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-import csv as csv
 import tkinter.messagebox
+import csv
 
 def geral():
 # essa função cria elementos gerais e inespecíficos da interface
@@ -14,21 +14,50 @@ def geral():
     # essa função abre o arquivo especificado pelo usuário
 
         try:  # verifica se o nome do arquivo é válido
-            arquivo = open(str(nome.get())+'.csv', "r")
+            file = open(str(nome.get())+'.csv', newline = '')
         except:
-            tkinter.messagebox.alert("arquivo não encontrado", "O arquivo que você procurou não foi encontrado. Verifique se o nome está escrito corretamente. \nNOTA: não é necessário incluir '.csv' no nome do arquivo")
+            tkinter.messagebox.showinfo("arquivo não encontrado", "O arquivo que você procurou não foi encontrado. Verifique se o nome está escrito corretamente. \nNOTA: não é necessário incluir '.csv' no nome do arquivo")
             return
+
+        # as listas paralelas são utilizadas para separar os dados
+        # em suas categorias para que sejam exibidos em ordem
+        nomes = []
+        email = []
+        cel = []
+        ra = []
+        curso = []
+        periodo = []
+        campus = []
+        area = []
+        subarea = []
+        qualidades = []
+        defeitos = []
+
+        reader = csv.reader(file, delimiter = ',')
+        for row in reader:
+            nomes.append(row[0]+'\n')
+            email.append(row[1]+'\n')
+            cel.append(row[2]+'\n')
+            ra.append(row[3]+'\n')
+            curso.append(row[4]+'\n')
+            periodo.append(row[5]+'\n')
+            campus.append(row[6]+'\n')
+            area.append(row[7]+'\n')
+            subarea.append(row[8]+'\n')
+            qualidades.append(row[9]+'\n')
+            defeitos.append(row[10]+'\n')
 
         # aqui cria-se uma nova janela onde os dados são exibidos
         display = tk.Toplevel()
-        display.geometry("1200x540")
+        display.geometry("1300x310")
 
-        dados = []
-        py = 10
-        c = 0
-        for i in arquivo:
-            tk.Message(display, text=i, width=1150).place(x=10, y=py)
-            py += 90
+        tk.Message(display, text=nomes, width=210, bg="lightblue").place(x=15, y=40)
+        tk.Message(display, text=email, width=320, bg="lightgreen").place(x=175, y=40)
+        tk.Message(display, text=cel, width=260, bg="lightyellow").place(x=465, y=40)
+        tk.Message(display, text=ra, width=100, bg="lightpink").place(x=635, y=40)
+        tk.Message(display, text=curso, width=600, bg="lightyellow").place(x=732, y=40)
+        tk.Message(display, text=periodo, width=60, bg="lightgreen").place(x=1043, y=40)
+        tk.Message(display, text=campus, width=300, bg="lightblue").place(x=1109, y=40)
 
     # essa parte adiciona o menu onde o usuário escreve o nome
     # do arquivo csv que deseja visualizar
