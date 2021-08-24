b966129da50ffd89648de4998a443f49cf0d25ba
myielin <mfernandamf114@gmail.com>
2021-08-24 02:49:16 -0300

    interface tk melhorada

--- code/interface.py
+++ code/interface.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.messagebox
 import geradorDados as gd
 import csv
+from functools import partial
 
 def geral():
 # essa função cria elementos gerais e inespecíficos da interface
@@ -60,15 +61,55 @@ def geral():
 
         # aqui cria-se uma nova janela onde os dados são exibidos
         display = tk.Toplevel()
-        display.geometry("1340x310")
+        display.geometry("1320x310")
 
-        tk.Message(display, text=str(nomes), width=210, bg="lightblue").place(x=15, y=40)
-        tk.Message(display, text=str(email), width=320, bg="lightgreen").place(x=175, y=40)
-        tk.Message(display, text=str(cel), width=260, bg="lightyellow").place(x=465, y=40)
-        tk.Message(display, text=str(ra), width=100, bg="lightpink").place(x=635, y=40)
-        tk.Message(display, text=str(curso), width=700, bg="lightyellow").place(x=732, y=40)
-        tk.Message(display, text=str(periodo), width=60, bg="lightgreen").place(x=1103, y=40)
-        tk.Message(display, text=str(campus), width=300, bg="lightblue").place(x=1173, y=40)
+        tk.Message(display, text=str(nomes), width=210, bg="lightblue").pack(side="left", padx=10)
+        tk.Message(display, text=str(email), width=320, bg="lightgreen").pack(side="left", padx=10)
+        tk.Message(display, text=str(cel), width=260, bg="lightyellow").pack(side="left", padx=10)
+        tk.Message(display, text=str(ra), width=100, bg="lightpink").pack(side="left", padx=10)
+        tk.Message(display, text=str(curso), width=700, bg="lightyellow").pack(side="left", padx=10)
+        tk.Message(display, text=str(campus), width=300, bg="lightblue").pack(side="left", padx=10)
+
+        def info(n):
+        # essa função mostra mais informações sobre cada participante individualmente
+
+            i = tk.Toplevel()
+            i.geometry("500x650")
+
+            candidato = tk.Label(i, text=str(nomes[n]))
+            candidato["font"] = ("Arial", "20", "bold")
+            candidato.place(x=40, y=20)
+
+            tk.Message(i, text="E-mail:", width=110, font="bold").place(x=15, y=80)
+            tk.Message(i, text=str(email[n]), width=400).place(x=125, y=80)
+            tk.Message(i, text=str("Whatsapp:"), width=130).place(x=15, y=110)
+            tk.Message(i, text=str(cel[n]), width=400).place(x=125, y=110)
+            tk.Message(i, text=str("RA:"), width=130).place(x=15, y=140)
+            tk.Message(i, text=str(ra[n]), width=400).place(x=125, y=140)
+            tk.Message(i, text=str("Curso:"), width=130).place(x=15, y=170)
+            tk.Message(i, text=str(curso[n]), width=1000).place(x=125, y=170)
+            tk.Message(i, text=str("Período:"), width=130).place(x=15, y=200)
+            tk.Message(i, text=str(periodo[n]), width=1000).place(x=125, y=200)
+            tk.Message(i, text=str("campus:"), width=130).place(x=15, y=230)
+            tk.Message(i, text=str(campus[n]), width=1000).place(x=125, y=230)
+            tk.Message(i, text=str("area:"), width=130).place(x=15, y=260)
+            tk.Message(i, text=str(area[n]), width=1000).place(x=125, y=260)
+            tk.Message(i, text=str("Sub-área:"), width=130).place(x=15, y=290)
+            tk.Message(i, text=str(subarea[n]), width=1000).place(x=125, y=290)
+            tk.Message(i, text=str("Qualidades:"), width=130).place(x=15, y=320)
+            tk.Message(i, text=str(qualidades[n]), width=340).place(x=125, y=320)
+            tk.Message(i, text=str("Defeitos:"), width=130).place(x=15, y=410)
+            tk.Message(i, text=str(defeitos[n]), width=340).place(x=125, y=410)
+
+            tk.Button(i, text="fechar", width=55, command= i.destroy, bg="lightblue").place(x=15, y=580)
+            tk.Button(i, text="sair da aplicação", width=55, command=quit, bg="lightgreen").place(x=15, y=610)
+
+
+
+        for n in range(len(nomes)):
+            py = 15 + n*25
+            a = nomes[n].split(" ")
+            if n >= 1: tk.Button(display, text=str(a[0]), width = 10, command=partial(info, n)).place(x=1170, y=py)
 
         file.close()
 
@@ -96,6 +137,7 @@ def geral():
     v = tk.Entry(frame, textvariable=arqv, width = 33).place(x=67, y=150)
     q = tk.Entry(frame, textvariable=qnt, width=6).place(x=431, y=150)
     b2 = tk.Button(frame, text="criar", width=56, command=criar, bg="lightgreen").place(x=10, y=175)
+    tk.Button(frame, text="fechar", width=56, command=quit, bg="lightpink").place(x=10, y=300)
 
 
     root.mainloop()
