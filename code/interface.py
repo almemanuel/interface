e76b93c3499390c38db92107cd4b95a7d9ddf322
Emanuel Almeida <emanuel.almeida1998@outlook.com>
2021-09-03 19:29:59 -0300

    correcoes botoes

--- code/interface.py
+++ code/interface.py
@@ -63,9 +63,9 @@ class interface:
                 count += 1
             elif n > 0: tk.Button(display, text=f"{n} - {str(primeiro_nome[0])}", width = 10, command=partial(self.info, n)).place(x=px, y=py)
 
-        tk.Button(display, text="adicionar novo", width=12*count, command=lambda: adicionar.addNovo(self.dados, str(self.arqv) ), bg="lightyellow").place(x=10, y=25 * (len(self.dados[0]) if len(self.dados[0]) <= 25 else 25) + 15)
+        tk.Button(display, text="Adicionar Novo", width=10, command=lambda: adicionar.addNovo(self.dados, str(self.arqv) ), bg="lightyellow").place(x=5 * (len(self.dados[0]) if len(self.dados[0]) < 25 else 25) + 10, y=0)
 
-        tk.Button(display, text="retornar", command=display.destroy, width=14*count, bg="lightpink").place(x=10, y=25 * (len(self.dados[0]) if len(self.dados[0]) <= 25 else 25) + 45)
+        tk.Button(display, text="Retornar", command=display.destroy, width=10, bg="lightpink").place(x=10, y=0)
 
     def abrir(self, *arg):
     # essa função abre o arquivo especificado pelo usuário
