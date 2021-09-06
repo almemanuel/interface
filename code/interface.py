ea8f8a46b70edc6dbfb8f01b5cb6a72b0d93f38a
myielin <mfernandamf114@gmail.com>
2021-09-06 14:40:08 -0300

    design

--- code/interface.py
+++ code/interface.py
@@ -57,25 +57,25 @@ class interface:
         print(texto)
 
     def botoes(self, display, dados):
-        px = 10
+        px = 20
         count = 0
         for n in range(len(dados[0])):
-            py = n*25
             primeiro_nome = dados[0][n].split(" ")
-            if n > 25: py = (n - 25*count)*25
-            if n % 25 == 0 and n > 0:
+            py = n*25
+            if n > 21: py = (n - 21*count)*25
+            if n % 21 == 0 and n > 0:
                 tk.Button(display, text=f"{n} - {str(primeiro_nome[0])}", width = 10, command=lambda:self.info(n, dados)).place(x=px, y=py)
                 px += 125
                 count += 1
             elif n > 0: tk.Button(display, text=f"{n} - {str(primeiro_nome[0])}", width = 10, command=lambda:self.info(n, dados)).place(x=px, y=py)
-            if n < 25: ymax = py + 10
+            if n < 21: ymax = py + 70
 
-        tk.Button(display, text="Novo", width=7, command=lambda: adicionar.addNovo(dados, str(self.arqv)), bg="lightyellow").place(x=10, y=ymax + 50)
-        tk.Button(display, text="Voltar", command=display.destroy, width=7, bg="lightpink").place(x=110, y=ymax+50)
+        tk.Button(display, text="Novo", width=7, command=lambda: adicionar.addNovo(dados, str(self.arqv)), bg="lightyellow").place(x=20, y=ymax)
+        tk.Button(display, text="Voltar", command=display.destroy, width=7, bg="lightpink").place(x=120, y=ymax)
         #filtro = self.selec
-        tk.Label(display, text="Para filtrar, selecione abaixo:").place(x = 10, y = ymax + 85)
-        ttk.Combobox(display, textvariable = self.selec, values=["Todos", "Administrativo", "Aviônica", "Mecânica", "Pesquisa e Extensão", "Computação"], width=12).place(x = 10, y = ymax + 105)
-        tk.Button(display, text="Filtrar", width = 3, command=self.teste).place(x = 145, y = ymax + 105)
+        tk.Label(display, text="Para filtrar, selecione abaixo:").place(x = 20, y = ymax + 35)
+        ttk.Combobox(display, textvariable = self.selec, values=["Todos", "Administrativo", "Aviônica", "Mecânica", "Pesquisa e Extensão", "Computação"], width=12).place(x = 20, y = ymax + 65)
+        tk.Button(display, text="Filtrar", width = 3, command=self.teste).place(x = 155, y = ymax + 60)
 
 
     def abrir(self, f):
@@ -99,7 +99,7 @@ class interface:
 
         file.close()
         display = tk.Toplevel()
-        display.geometry(f"{120 * (len(self.dados[0])//25 if len(self.dados[0]) <= 225 else 10) + 100}x{25 * (len(self.dados[0]) if len(self.dados[0]) < 25 else 25) + 150}")
+        display.geometry(f"{105 * (len(self.dados[0])//21 if len(self.dados[0]) <= 225 else 10) + 250}x{20 * (len(self.dados[0]) if len(self.dados[0]) < 21 else 25) + 180}")
 
         self.botoes(display, self.dados)
 
